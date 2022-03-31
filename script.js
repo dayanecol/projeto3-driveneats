e24502bad9f6f28af0aca24c5e40500a02f5fc2e
dayane <dayanecarolina.oliveira@gmail.com>
2022-03-31 00:13:56 -0300

    Versao 31_03 00h13

---
+++ script.js
@@ -0,0 +1,89 @@
+function frango(){
+    document.getElementById("frango").style.borderColor= "green"
+    document.getElementById("pizza").style.borderColor= "#fff"
+    document.getElementById("hamburguer").style.borderColor= "#fff"
+    document.getElementById("carne").style.borderColor= "#fff"
+}
+
+function pizza(){
+    document.getElementById("pizza").style.borderColor= "green"
+    document.getElementById("frango").style.borderColor= "#fff"
+    document.getElementById("hamburguer").style.borderColor= "#fff"
+    document.getElementById("carne").style.borderColor= "#fff"
+}
+
+function hamburguer(){
+    document.getElementById("hamburguer").style.borderColor= "green"
+    document.getElementById("pizza").style.borderColor= "#fff"
+    document.getElementById("frango").style.borderColor= "#fff"
+    document.getElementById("carne").style.borderColor= "#fff"
+}
+
+function carne(){
+    document.getElementById("carne").style.borderColor= "green"
+    document.getElementById("hamburguer").style.borderColor= "#fff"
+    document.getElementById("pizza").style.borderColor= "#fff"
+    document.getElementById("frango").style.borderColor= "#fff"
+}
+
+
+
+
+
+
+function coca(){
+    document.getElementById("coca").style.borderColor= "green"
+    document.getElementById("cerveja").style.borderColor= "#fff"
+    document.getElementById("suco").style.borderColor= "#fff"
+    document.getElementById("agua").style.borderColor= "#fff"
+}
+
+function cerveja(){
+    document.getElementById("cerveja").style.borderColor= "green"
+    document.getElementById("coca").style.borderColor= "#fff"
+    document.getElementById("suco").style.borderColor= "#fff"
+    document.getElementById("agua").style.borderColor= "#fff"
+}
+
+function suco(){
+    document.getElementById("suco").style.borderColor= "green"
+    document.getElementById("cerveja").style.borderColor= "#fff"
+    document.getElementById("coca").style.borderColor= "#fff"
+    document.getElementById("agua").style.borderColor= "#fff"
+}
+
+function agua(){
+    document.getElementById("agua").style.borderColor= "green"
+    document.getElementById("suco").style.borderColor= "#fff"
+    document.getElementById("cerveja").style.borderColor= "#fff"
+    document.getElementById("coca").style.borderColor= "#fff"
+}
+
+
+function pudim(){
+    document.getElementById("pudim").style.borderColor= "green"
+    document.getElementById("bolo").style.borderColor= "#fff"
+    document.getElementById("macarron").style.borderColor= "#fff"
+    document.getElementById("brigadeiro").style.borderColor= "#fff"
+}
+
+function bolo(){
+    document.getElementById("bolo").style.borderColor= "green"
+    document.getElementById("pudim").style.borderColor= "#fff"
+    document.getElementById("macarron").style.borderColor= "#fff"
+    document.getElementById("brigadeiro").style.borderColor= "#fff"
+}
+
+function macarron(){
+    document.getElementById("macarron").style.borderColor= "green"
+    document.getElementById("bolo").style.borderColor= "#fff"
+    document.getElementById("pudim").style.borderColor= "#fff"
+    document.getElementById("brigadeiro").style.borderColor= "#fff"
+}
+
+function brigadeiro(){
+    document.getElementById("brigadeiro").style.borderColor= "green"
+    document.getElementById("macarron").style.borderColor= "#fff"
+    document.getElementById("bolo").style.borderColor= "#fff"
+    document.getElementById("pudim").style.borderColor= "#fff"
+}
